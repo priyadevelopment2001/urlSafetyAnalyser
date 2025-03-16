ec06ddb71fb3ad504179875e4940065a900ae9aa
shivamtechstack <devshivamyadav1604@gmail.com>
2025-03-16 16:50:20 +0530

    -screenshotPreview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:convertor-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.google.code.gson:gson:2.8.9")
 
 }
--- app/src/main/java/com/priya/urlsafetyanalyser/MainActivity.kt
+++ app/src/main/java/com/priya/urlsafetyanalyser/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.priya.urlsafetyanalyser
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
+import com.priya.urlsafetyanalyser.utils.WebViewScreenshotService
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var submitButton : Button
     private lateinit var resultText : TextView
-    private lateinit var urlEditText : TextInputEditText
+    private lateinit var urlEditText : EditText
+    private lateinit var urlPreview : ImageView
+    private lateinit var errorText : TextView
+    private lateinit var urlPreviewText : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +36,21 @@ class MainActivity : AppCompatActivity() {
 
         submitButton =findViewById<Button>(R.id.submitButton)
         resultText=findViewById<TextView>(R.id.resultTextView)
-        urlEditText=findViewById<TextInputEditText>(R.id.urlEditTextBox)
+        urlEditText=findViewById<EditText>(R.id.urlEditTextBox)
+        urlPreview=findViewById<ImageView>(R.id.URLPreview)
+        errorText=findViewById<TextView>(R.id.errorTextView)
+        urlPreviewText=findViewById<TextView>(R.id.urlPreviewTextView)
 
         submitButton.setOnClickListener{
             val url =urlEditText.text.toString().trim()
 
             if (url.isNotEmpty()){
+                errorText.visibility = View.GONE
                 analyseURl(url)
+                loadPreview(url)
             }else{
-                resultText.text = "Please enter a valid URl"
+               errorText.visibility = View.VISIBLE
+                errorText.text = "Please enter a URL"
             }
         }
 
@@ -56,19 +69,39 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }catch (e : Exception){
-
+                e.printStackTrace()
             }
+
         }
+        thread.start()
     }
 
-    private fun checkWithPhishTank(url: String) {}
+    private fun checkWithPhishTank(url: String) {
+
+    }
 
     private fun checkWithCustomFilter(url: String) : String {
         val maliciousWords = listOf("malware","Malware","phishing","attack", "hacked","danger","virus")
         return if (maliciousWords.any { url.contains(it, ignoreCase = true) }){
             "Unsafe URL detected by custom filter"
         }else{
-            "NO suspicious word found in URl."
+            "NO suspicious word found in URL."
+        }
+    }
+
+    private fun loadPreview(url: String) {
+        WebViewScreenshotService.loadUrlInBackground(this, url) { screenshot ->
+            runOnUiThread {
+                if (screenshot != null) {
+                    urlPreview.setImageBitmap(screenshot)
+                    urlPreviewText.visibility = View.VISIBLE
+                    urlPreview.visibility = View.VISIBLE
+
+                } else {
+                    urlPreview.visibility = View.GONE
+                    urlPreviewText.visibility = View.GONE
+                }
+            }
         }
     }
 }
